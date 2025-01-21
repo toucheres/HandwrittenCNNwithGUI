7cc2771cf5c70d2bd9ce56cc14d72a7720724d9e
toucheres <761844639@qq.com>
2025-01-21 18:31:45 +0800

    引入基本qt库，生成窗口

--- exetest/include/main.h
+++ exetest/include/main.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <mainWindow.h>
 #include <bmp.h>
 #include <dynamic_lib.h>
 void createBlackWhiteBMP(const std::vector<int> &bw, const std::string &path);
